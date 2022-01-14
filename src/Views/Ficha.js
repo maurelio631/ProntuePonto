6148425f59e48e5d0c5022bc1bd918b7a5ffc61d
Your Name <you@example.com>
2022-01-13 21:30:22 -0300

    alterações na ficha

--- src/Views/Ficha.js
+++ src/Views/Ficha.js
@@ -1,11 +1,29 @@
 import react from "react";
 import styles from "./Ficha.module.css";
 import LogoPep from "../Assets/img/LogoPeP.png";
+import CalendarIcon from "../Assets/img/CalendarIcon.png"
+import PatientIcon from "../Assets/img/PatientIcon.png";
+import MoneyIcon from "../Assets/img/MoneyIcon.png";
+import SettingsIcon from "../Assets/img/SettingsIcon.png";
 export default function Ficha(props) {
     return (
         <div className={styles.wrapper}>
             <div className={styles.sidebar}>
-                <img src={LogoPep} className={styles.logo}></img>
+                <div>
+                    <img src={LogoPep} className={styles.logo}></img>
+                </div>
+                <div>
+                    <img src={CalendarIcon} className={styles.calendarIcon}></img>
+                </div>
+                <div>
+                    <img src={PatientIcon} className={styles.patientIcon}></img>
+                </div>
+                <div>
+                    <img src={MoneyIcon} className={styles.sidebarImage}></img>
+                </div>
+                <div>
+                    <img src={SettingsIcon} className={styles.sidebarImage}></img>
+                </div>
             </div>
             <div className={styles.ficha}>
                 <div className={styles.header}>
@@ -21,51 +39,57 @@ export default function Ficha(props) {
                 <div className={styles.stepNameHighlight}>
                     <h4>Auto-Avaliação - Prontuário de atendimento de Quiropraxia</h4>
                 </div>
-                <div className={styles.formInfosPessoais}>
-                    <div>
-                        <div >
-                            <label>Nome</label>
-                            <input placeholder="Insira seu nome"></input>
-                        </div>
-                        <div>
-                            <div>
-                                <label>Data de nascimento</label>
-                                <input placeholder="Insira sua data de nascimento"></input>
-                            </div>
-                            <div>
-                                <label> Idade:</label><b>21</b>
-                            </div>
-                        </div>
+                <form className={styles.formInfosPessoais} action="" method="POST">
+                    <div className={styles.formGroup}>
+                        <label>Nome</label>
+                        <input placeholder="Insira seu nome"></input>
                     </div>
-                    <div>
+
+                    <div className={styles.formGroup}>
+                        <label>Data de nascimento</label>
+                        <input type={"date"} placeholder="Insira sua data de nascimento"></input>
+                    </div>
+
+                    <div className={styles.formGroup}>
+                        <label> Idade:</label><b>00</b>
+                    </div>
+
+                    <div className={styles.formGroup}>
                         <label>Profissão</label>
                         <input placeholder="Ex: Desenvolvedor"></input>
                     </div>
-                    <div>
+
+                    <div className={styles.formGroup}>
                         <label>Telefone/Celular</label>
                         <input></input>
                     </div>
-                    <div>
+                    <div className={styles.formGroup}>
                         <label>CPF</label>
                         <input placeholder="XXX.XXX.XXX-XX"></input>
                     </div>
-                    <div>
+                    <div className={styles.formGroup}>
                         <label>E-mail</label>
                         <input placeholder="seuemail@aqui.com"></input>
                     </div>
-                    <div>
+                    <div className={styles.formGroup}>
+
                         <label>Endereço</label>
                         <input></input>
                     </div>
-                    <div>
+                    <div className={styles.formGroup}>
                         <label>Como nos conheceu?</label>
                         <select>
                             <option selected>Amigo</option>
                             <option>Outros</option>
                         </select>
                     </div>
-                </div>
-            </div>
-        </div>
+                    <div className={styles.fichaFooter}>
+                        <button className={styles.cancelButton}>Cancelar</button>
+                        <div className={styles.progress}>Progresso da ficha</div>
+                        <button className={styles.nextButton}>Continuar</button>
+                    </div>
+                </form>
+            </div >
+        </div >
     )
 }
